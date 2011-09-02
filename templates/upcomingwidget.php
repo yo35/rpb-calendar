a55cfc329d4bbc5edb2c37a079a99a192a46071e
Yoann Le Montagner <yo35@melix.net>
2011-09-02 19:57:38 +0000

    Amélioration affichage date'

--- templates/upcomingwidget.php
+++ templates/upcomingwidget.php
@@ -40,13 +40,23 @@
 		);
 
 		// Date range label
-		if($date_range->event_begin==$date_range->event_end) {
-			$date_range_label = date_i18n(get_option('date_format'), strtotime($date_range->event_begin));
+		$event_begin = strtotime($date_range->event_begin);
+		$event_end   = strtotime($date_range->event_end  );
+		$day_begin   = date('j', $event_begin);
+		$day_end     = date('j', $event_end  );
+		$month_begin = date('n', $event_begin);
+		$month_end   = date('n', $event_end  );
+		$year_begin  = date('Y', $event_begin);
+		$year_end    = date('Y', $event_end  );
+		if($event_begin==$event_end) {
+			$date_range_label = $day_begin.' '.rpbcalendar_month_info('name', $month_begin);
 		} else {
-			$current_begin    = date_i18n(get_option('date_format'), strtotime($date_range->event_begin));
-			$current_end      = date_i18n(get_option('date_format'), strtotime($date_range->event_end  ));
-			$date_range_label = sprintf(__('From %1$s to %2$s', 'rpbcalendar'),
-				$current_begin, $current_end);
+			$range_begin = $day_begin;
+			if($month_begin!=$month_end || $year_begin!=$year_end) {
+				$range_begin .= ' '.rpbcalendar_month_info('name', $month_begin);
+			}
+			$range_end = $day_end.' '.rpbcalendar_month_info('name', $month_end);
+			$date_range_label = sprintf(__('From %1$s to %2$s', 'rpbcalendar'), $range_begin, $range_end);
 		}
 
 		// Display events
