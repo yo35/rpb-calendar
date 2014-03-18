701beec66cdd5a0c207412438fd0c5fe930a3a86
Yoann Le Montagner <yo35@melix.net>
2014-03-18 17:12:22 +0100

    Abstract view class

---
+++ views/abstractview.php
@@ -0,0 +1,53 @@
+<?php
+/******************************************************************************
+ *                                                                            *
+ *    This file is part of RPB Calendar, a Wordpress plugin.                  *
+ *    Copyright (C) 2014  Yoann Le Montagner <yo35 -at- melix.net>            *
+ *                                                                            *
+ *    This program is free software: you can redistribute it and/or modify    *
+ *    it under the terms of the GNU General Public License as published by    *
+ *    the Free Software Foundation, either version 3 of the License, or       *
+ *    (at your option) any later version.                                     *
+ *                                                                            *
+ *    This program is distributed in the hope that it will be useful,         *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *    GNU General Public License for more details.                            *
+ *                                                                            *
+ *    You should have received a copy of the GNU General Public License       *
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *                                                                            *
+ ******************************************************************************/
+
+
+/**
+ * Base class for the views used by the RPB Calendar plugin.
+ */
+abstract class RPBCalendarAbstractView
+{
+	private $model;
+
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct($model)
+	{
+		$this->model = $model;
+	}
+
+
+	/**
+	 * Method called the controller to display the view.
+	 */
+	public abstract function display();
+
+
+	/**
+	 * Model associated to the view.
+	 */
+	public function getModel()
+	{
+		return $this->model;
+	}
+}
