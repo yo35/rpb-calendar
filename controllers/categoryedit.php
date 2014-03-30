99b7176322ab938a50e3c1796b136a9641a4f9b3
Yoann Le Montagner <yo35@melix.net>
2014-03-30 18:22:25 +0200

    MVC to customize the event category edition form

---
+++ controllers/categoryedit.php
@@ -0,0 +1,48 @@
+<?php
+/******************************************************************************
+ *                                                                            *
+ *    This file is part of RPB Calendar, a Wordpress plugin.                  *
+ *    Copyright (C) 2014  Yoann Le Montagner <yo35 -at- melix.net>            *
+ *                                                                            *
+ *    This program is free software: you can redistribute it and/or modify    *
+ *    it under the terms of the GNU General Public License as published by    *
+ *    the Free Software Foundation, either version 3 of the License, or       *
+ *    (at your option) any later version.                                     *
+ *                                                                            *
+ *    This program is distributed in the hope that it will be useful,         *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *    GNU General Public License for more details.                            *
+ *                                                                            *
+ *    You should have received a copy of the GNU General Public License       *
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *                                                                            *
+ ******************************************************************************/
+
+
+require_once(RPBCALENDAR_ABSPATH . 'controllers/abstractcontroller.php');
+
+
+/**
+ * Customize the form used to edit an event category.
+ */
+class RPBCalendarControllerCategoryEdit extends RPBCalendarAbstractController
+{
+	private $category;
+
+
+	public function __construct($category)
+	{
+		parent::__construct('CategoryEdit');
+		$this->category = $category;
+	}
+
+
+	public function run()
+	{
+		$model = $this->getModel();
+		$model->setCategoryID($category->ID);
+		$model->useTemplate('ColorField');
+		$this->getView()->display();
+	}
+}
