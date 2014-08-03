f7519713618a76ee2205bc295b866fe5a3d0e58a
Yoann Le Montagner <yo35@melix.net>
2014-08-03 18:42:16 +0200

    Format date/date-range helper functions

--- helpers/date.php
+++ helpers/date.php
@@ -40,4 +40,75 @@ abstract class RPBCalendarHelperDate
 		}
 		return self::$today;
 	}
+
+
+	/**
+	 * Format a date into a human-readable string.
+	 *
+	 * @param int $date
+	 * @param boolean $showWeekday
+	 * @param boolean $showYear
+	 * @return string
+	 */
+	public static function format($date, $showWeekday, $showYear)
+	{
+		global $wp_locale;
+		$date = getdate($date);
+
+		$res = $showWeekday ? ($showYear ?
+			/*i18n Date format, full (e.g. "Sunday, August 3, 2014")    */ __('%w$s, %m$s %d$s, %y$s', 'rpbcalendar') :
+			/*i18n Date format, without year (e.g. "Sunday, August, 3") */ __('%w$s, %m$s %d$s'      , 'rpbcalendar')
+		) : ($showYear ?
+			/*i18n Date format, without weekday (e.g. "August 3, 2014") */ __('%m$s %d$s, %y$s'      , 'rpbcalendar') :
+			/*i18n Date format, only month and day (e.g. "August 3")    */ __('%m$s %d$s'            , 'rpbcalendar')
+		);
+
+		$res = preg_replace('/%y\\$s/', $date['year']                         , $res);
+		$res = preg_replace('/%m\\$s/', $wp_locale->get_month($date['mon'])   , $res);
+		$res = preg_replace('/%d\\$s/', $date['mday']                         , $res);
+		$res = preg_replace('/%w\\$s/', $wp_locale->get_weekday($date['wday']), $res);
+		return $res;
+	}
+
+
+	/**
+	 * Format a date range into a human-readable string.
+	 *
+	 * @param int $from
+	 * @param int $to
+	 * @return string
+	 */
+	public static function formatRange($from, $to)
+	{
+		if($from === $to) {
+			return self::format($from, false, true);
+		}
+
+		global $wp_locale;
+		$date1 = getdate($from);
+		$date2 = getdate($to  );
+
+		// First case: the begin date and the end date are within the same month.
+		if($date1['mon'] === $date2['mon'] && $date1['year'] === $date2['year']) {
+			$res = /*i18n Date range format, same begin/end month */ __('%m1$s %d1$s–%d2$s, %y1$s', 'rpbcalendar');
+		}
+
+		// Second case: the begin and the end date are within the same year.
+		else if($date1['year'] === $date2['year']) {
+			$res = /*i18n Date range format, same begin/end year */ __('%m1$s %d1$s – %m2$s %d2$s, %y1$s', 'rpbcalendar');
+		}
+
+		// General case
+		else {
+			$res = /*i18n Date range format, large range */ __('%m1$s %d1$s, %y1$s – %m2$s %d2$s, %y2$s', 'rpbcalendar');
+		}
+
+		$res = preg_replace('/%y1\\$s/', $date1['year']                         , $res);
+		$res = preg_replace('/%m1\\$s/', $wp_locale->get_month($date1['mon'])   , $res);
+		$res = preg_replace('/%d1\\$s/', $date1['mday']                         , $res);
+		$res = preg_replace('/%y2\\$s/', $date2['year']                         , $res);
+		$res = preg_replace('/%m2\\$s/', $wp_locale->get_month($date2['mon'])   , $res);
+		$res = preg_replace('/%d2\\$s/', $date2['mday']                         , $res);
+		return $res;
+	}
 }
